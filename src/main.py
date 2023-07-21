2806f3f710980139cd630efe363eb04ed80e1e30
samson-bakos <samsonbakos@hotmail.com>
2023-07-21 13:34:42 -0700

    Add letter entry

--- src/main.py
+++ src/main.py
@@ -9,6 +9,19 @@ body4 = st.container()
 body5 = st.container()
 body6 = st.container()
 
+st.markdown(
+    """
+    <style>
+    .stButton>button {
+        width: 100%;
+        box-sizing: border-box;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+
 with header:
     st.title("Wordle Cheater")
     st.text("Statistical Solver for the Daily Wordle Puzzle")
@@ -22,15 +35,28 @@ states = [
 num_states = len(states)
 
 with body1:
-    col11, col12, col13, col14, col15 = st.columns(5)
+    (
+        fill1,
+        fill2,
+        col11,
+        col12,
+        col13,
+        col14,
+        col15,
+        fill3,
+        fill4,
+    ) = st.columns(9)
 
     # Initialize session states and checkboxes
     for i, col in enumerate([col11, col12, col13, col14, col15], start=1):
         session_state_key = f"body1_current_state{i}"
         checkbox_state_key = f"body1_checkbox_state{i}"
+        text_state_key = f"body1_text_state{i}"
 
         current_state = st.session_state.get(session_state_key, 0)
         checkbox_state = st.session_state.get(checkbox_state_key, False)
+
+        letter1 = col.text_input("", "", 1, key=f"body1_text{i}")
 
         if col.button("", key=f"body1_button{i}"):
             current_state = (current_state + 1) % num_states
@@ -39,19 +65,22 @@ with body1:
 
         selected_color = states[current_state]["color"]
 
-        colored_checkbox = f'<div style="background-color: {selected_color}; width: 50px; height: 50px;"></div>'
+        colored_checkbox = f'<div style="background-color: {selected_color}; width: 68px; height: 40px;"></div>'
         col.markdown(colored_checkbox, unsafe_allow_html=True)
 
 with body2:
-    col21, col22, col23, col24, col25 = st.columns(5)
+    fill21, fill22, col21, col22, col23, col24, col25, fill23, fill24 = st.columns(9)
 
     # Initialize session states and checkboxes
     for i, col in enumerate([col21, col22, col23, col24, col25], start=1):
         session_state_key = f"body2_current_state{i}"
         checkbox_state_key = f"body2_checkbox_state{i}"
+        text_state_key = f"body2_text_state{i}"
 
         current_state = st.session_state.get(session_state_key, 0)
         checkbox_state = st.session_state.get(checkbox_state_key, False)
+
+        letter2 = col.text_input("", "", 1, key=f"body2_text{i}")
 
         if col.button("", key=f"body2_button{i}"):
             current_state = (current_state + 1) % num_states
@@ -60,19 +89,22 @@ with body2:
 
         selected_color = states[current_state]["color"]
 
-        colored_checkbox = f'<div style="background-color: {selected_color}; width: 50px; height: 50px;"></div>'
+        colored_checkbox = f'<div style="background-color: {selected_color}; width: 68px; height: 40px;"></div>'
         col.markdown(colored_checkbox, unsafe_allow_html=True)
 
 with body3:
-    col31, col32, col33, col34, col35 = st.columns(5)
+    fill31, fill32, col31, col32, col33, col34, col35, fill33, fill34 = st.columns(9)
 
     # Initialize session states and checkboxes
     for i, col in enumerate([col31, col32, col33, col34, col35], start=1):
         session_state_key = f"body3_current_state{i}"
         checkbox_state_key = f"body3_checkbox_state{i}"
+        text_state_key = f"body3_text_state{i}"
 
         current_state = st.session_state.get(session_state_key, 0)
         checkbox_state = st.session_state.get(checkbox_state_key, False)
+
+        letter3 = col.text_input("", "", 1, key=f"body3_text{i}")
 
         if col.button("", key=f"body3_button{i}"):
             current_state = (current_state + 1) % num_states
@@ -81,19 +113,22 @@ with body3:
 
         selected_color = states[current_state]["color"]
 
-        colored_checkbox = f'<div style="background-color: {selected_color}; width: 50px; height: 50px;"></div>'
+        colored_checkbox = f'<div style="background-color: {selected_color}; width: 68px; height: 40px;"></div>'
         col.markdown(colored_checkbox, unsafe_allow_html=True)
 
 with body4:
-    col41, col42, col43, col44, col45 = st.columns(5)
+    fill41, fill42, col41, col42, col43, col44, col45, fill43, fill44 = st.columns(9)
 
     # Initialize session states and checkboxes
     for i, col in enumerate([col41, col42, col43, col44, col45], start=1):
         session_state_key = f"body4_current_state{i}"
         checkbox_state_key = f"body4_checkbox_state{i}"
+        text_state_key = f"body4_text_state{i}"
 
         current_state = st.session_state.get(session_state_key, 0)
         checkbox_state = st.session_state.get(checkbox_state_key, False)
+
+        letter4 = col.text_input("", "", 1, key=f"body4_text{i}")
 
         if col.button("", key=f"body4_button{i}"):
             current_state = (current_state + 1) % num_states
@@ -102,19 +137,24 @@ with body4:
 
         selected_color = states[current_state]["color"]
 
-        colored_checkbox = f'<div style="background-color: {selected_color}; width: 50px; height: 50px;"></div>'
+        colored_checkbox = f'<div style="background-color: {selected_color}; width: 68px; height: 40px;"></div>'
         col.markdown(colored_checkbox, unsafe_allow_html=True)
 
     with body5:
-        col51, col52, col53, col54, col55 = st.columns(5)
+        fill51, fill52, col51, col52, col53, col54, col55, fill53, fill54 = st.columns(
+            9
+        )
 
         # Initialize session states and checkboxes
         for i, col in enumerate([col51, col52, col53, col54, col55], start=1):
             session_state_key = f"body5_current_state{i}"
             checkbox_state_key = f"body5_checkbox_state{i}"
+            text_state_key = f"body5_text_state{i}"
 
             current_state = st.session_state.get(session_state_key, 0)
             checkbox_state = st.session_state.get(checkbox_state_key, False)
+
+            letter5 = col.text_input("", "", 1, key=f"body5_text{i}")
 
             if col.button("", key=f"body5_button{i}"):
                 current_state = (current_state + 1) % num_states
@@ -123,19 +163,24 @@ with body4:
 
             selected_color = states[current_state]["color"]
 
-            colored_checkbox = f'<div style="background-color: {selected_color}; width: 50px; height: 50px;"></div>'
+            colored_checkbox = f'<div style="background-color: {selected_color}; width: 68px; height: 40px;"></div>'
             col.markdown(colored_checkbox, unsafe_allow_html=True)
 
     with body6:
-        col61, col62, col63, col64, col65 = st.columns(5)
+        fill61, fill62, col61, col62, col63, col64, col65, fill63, fill64 = st.columns(
+            9
+        )
 
         # Initialize session states and checkboxes
         for i, col in enumerate([col61, col62, col63, col64, col65], start=1):
             session_state_key = f"body6_current_state{i}"
             checkbox_state_key = f"body6_checkbox_state{i}"
+            text_state_key = f"body6_text_state{i}"
 
             current_state = st.session_state.get(session_state_key, 0)
             checkbox_state = st.session_state.get(checkbox_state_key, False)
+
+            letter6 = col.text_input("", "", 1, key=f"body6_text{i}")
 
             if col.button("", key=f"body6_button{i}"):
                 current_state = (current_state + 1) % num_states
@@ -144,7 +189,11 @@ with body4:
 
             selected_color = states[current_state]["color"]
 
-            colored_checkbox = f'<div style="background-color: {selected_color}; width: 50px; height: 50px;"></div>'
+            colored_checkbox = f'<div style="background-color: {selected_color}; width: 68px; height: 40px;"></div>'
             col.markdown(colored_checkbox, unsafe_allow_html=True)
 
     st.sidebar.header("Recommended Word:")
+
+    # ADD "DONE" BUTTON TO SIDE OF EACH COLUMN
+    # ADD USAGE SECTION TO SIDEBAR
+    # INTEGRATE LOGICAL CODE
